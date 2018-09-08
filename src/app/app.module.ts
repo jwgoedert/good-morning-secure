8b360294986bef4205f7968a1273a8ccae293e24
James Wesley Goedert <jwgoedert@MacBook-Pro-10.local>
2018-09-08 11:37:36 -0500

    Preliminary commit with image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ImageBoxComponent } from './image-box/image-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageBoxComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
